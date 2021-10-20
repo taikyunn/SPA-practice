bb1cf152eb7f03e27e012ef242082c9b20961cb1
taikyunn <taichi_141@me.com>
2021-10-21 08:24:38 +0900

    commit

--- server.go
+++ server.go
@@ -13,36 +13,38 @@ import (
 )
 
 func main() {
-	// サーバーの起動
+	// サーバーを起動する
 	serve()
 }
 
 func serve() {
-	r := gin.Default()
+	// デフォルトのミドルウェアでginのルーターを作成
+	// Logger と アプリケーションクラッシュをキャッチするRecoveryミドルウェア を保有しています
+	router := gin.Default()
 
-	// ファイルのパス指定
-	r.Static("/views", "./views")
+	// 静的ファイルのパスを指定
+	router.Static("/views", "./views")
 
 	// ルーターの設定
 	// URLへのアクセスに対して静的ページを返す
-	r.StaticFS("/shoppingapp", http.Dir("./views/static"))
+	router.StaticFS("/shoppingapp", http.Dir("./views/static"))
 
 	// 全ての商品情報のJSONを返す
-	r.GET("/fetchAllProducts", controller.FetchAllProducts)
+	router.GET("/fetchAllProducts", controller.FetchAllProducts)
 
-	// 1つの商品情報の状態のJSONを返す
-	r.GET("/fetchProduct", controller.FindProduct)
+	// １つの商品情報の状態のJSONを返す
+	router.GET("/fetchProduct", controller.FindProduct)
 
 	// 商品情報をDBへ登録する
-	r.GET("/addProduct", controller.AddProduct)
+	router.POST("/addProduct", controller.AddProduct)
 
 	// 商品情報の状態を変更する
-	r.POST("/changeStateProduct", controller.ChangeStateProduct)
+	router.POST("/changeStateProduct", controller.ChangeStateProduct)
 
 	// 商品情報を削除する
-	r.POST("/deleteProduct", controller.DeleteProduct)
+	router.POST("/deleteProduct", controller.DeleteProduct)
 
-	if err := r.Run(); err != nil {
+	if err := router.Run(":8080"); err != nil {
 		log.Fatal("Server Run Failed.: ", err)
 	}
 }
